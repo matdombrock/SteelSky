fe1c0082aec73ab01226b1b75ccd11bb95793791
matdombrock <matdombrock@gmail.com>
2021-04-27 13:21:43 +0000

    Cleanup

--- steelsky.js
+++ steelsky.js
@@ -23,18 +23,11 @@ const footer = fs.readFileSync(layoutPath+'/footer.html','utf-8');
 const theme = fs.readFileSync(layoutPath+'/theme.css','utf-8');
 const highlightStyleCSS = fs.readFileSync(__dirname+'/node_modules/highlight.js/styles/'+highlightStyle+'.css','utf-8');
 
-let outList = [];
-
-// After requiring the module, use it as extension
 let converter = new showdown.Converter({
-    // That's it
     extensions: [showdownHighlight({
-        // Whether to add the classes to the <pre> tag
         pre: true
     })]
 });
-
-
 
 function traverse(path, rootPath, list = []){
   const listing = fs.readdirSync(path);
@@ -103,19 +96,11 @@ if (!fs.existsSync(outPath)){
   fs.mkdirSync(outPath);
 }
 
+let outList = [];
 for(let item of list){
   convert(item);
 }
 
-// Generate Directory Listing
-// let listHtml = '<div class="ss-listing-area">';
-// console.log(outList);
-// for(let item of outList){
-//   listHtml += '<a class="ss-listing" href="'+item+'">'+item+'</a><br>';
-// }
-// listHtml += '</div>';
-// const html = header + '<style>' + highlightStyle +'</style>' + listHtml + footer + '<style>' + theme +'</style>';
-// fs.writeFileSync(__dirname+'/out/listing.html', html);
 fs.writeFileSync(outPath+'/listing.json', JSON.stringify(outList, null, 2));
 
 

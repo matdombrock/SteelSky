9378482da2461299c5f5cf015ff8a1ad556e6795
Mathieu Dombrock <matdombrock@gmail.com>
2023-05-02 02:32:11 -0700

    rewrite as oop ts

--- resources/config.example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sourcePath: __dirname + '/../SteelSkyExample/source',
-  layoutPath: __dirname + '/../SteelSkyExample/layout',
-  highlightStyle: 'a11y-dark',
-  outPath: __dirname + '/../SteelSkyExample/out',
-};
